f17108f0afdfd7b63cfbe9c273141d366b979bc2
Prathamesh0901 <prathameshmane3622@gmail.com>
2025-11-26 01:04:08 +0000

    added port

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request 
+import os
  
 app = Flask(__name__) 
  
@@ -32,4 +33,4 @@ def get_user(user_id):
     return jsonify({"error": "User not found"}), 404 
  
 if __name__ == "__main__": 
-    app.run(debug=True, port=8080)
+    app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000))
